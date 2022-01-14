de5321b31f2307ae687dddba7dafed21827df29b
haydencordeiro <cordeirohayden@gmail.com>
2022-01-14 11:49:37 +0530

    updated page object model

--- features/features_files/pages/landing_page.rb
+++ features/features_files/pages/landing_page.rb
@@ -1,17 +1,20 @@
 class LandingPage
-
-
+    def initialize
+        @pop_up_close_button_selector=[:css, 'button[class="_2KpZ6l _2doB4z"]']
+        @search_bar_selector=[:css, 'input[class="_3704LK"]']
+        @search_button_selector=[:css, 'button[class="L0Z3Pu"]']
+    end
     def open_website url
         $driver.navigate.to  url
     end
     def close_pop_up
-        close_pop_up=$driver.find_element(:css, 'button[class="_2KpZ6l _2doB4z"]')
+        close_pop_up=$driver.find_element *@pop_up_close_button_selector
         $wait.until {close_pop_up.displayed?}
         close_pop_up.click
     end
     def search_for_product product
-        search_bar = $driver.find_element(:css, 'input[class="_3704LK"]')
+        search_bar = $driver.find_element *@search_bar_selector
         search_bar.send_keys  product
-        $driver.find_element(:css, 'button[class="L0Z3Pu"]').click
+        $driver.find_element(*@search_button_selector).click
     end
 end
--- features/features_files/pages/search_results_page.rb
+++ features/features_files/pages/search_results_page.rb
@@ -1,8 +1,10 @@
 class ResultsPage
-
+    def initialize
+        @results_div_selector=[:css, '._1AtVbE']
+    end
     def verify_search_results 
         sleep(1)
-        results=$driver.find_elements(:css, '._1AtVbE')
+        results=$driver.find_elements(*@results_div_selector)
         results.length()>0  
 
     end
