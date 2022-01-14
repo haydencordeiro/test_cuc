6bbf8dd61121ee13d1e4448d0e4b9dea4748a7ed
haydencordeiro <cordeirohayden@gmail.com>
2022-01-14 12:16:59 +0530

    updated code

--- features/features_files/pages/landing_page.rb
+++ features/features_files/pages/landing_page.rb
@@ -8,7 +8,6 @@ class LandingPage
         $driver.navigate.to  url
     end
     def close_pop_up
-        sleep(2)
         close_pop_up_button=$driver.find_element *@pop_up_close_button_selector
         $wait.until {close_pop_up_button.displayed?}
         close_pop_up_button.click
